1c056e991732f4695b9fcaf8ecac01eb63411abe
Katie Hopkins <HopkinsKV@gmail.com>
2022-01-20 14:04:53 -0600

    GeoJson

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,26 @@
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: '1, 5',
+}).addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+}).addTo(map);
